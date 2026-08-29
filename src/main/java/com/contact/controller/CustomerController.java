40164ab1284e5e69fab040ee921f4fa688c03c2c
Harsh Rathore <rathorehrharsh5@gmail.com>
2026-08-29 20:58:27 +0530

    Complete Controller Functionality for getting contact

--- src/main/java/com/contact/controller/CustomerController.java
+++ src/main/java/com/contact/controller/CustomerController.java
@@ -6,6 +6,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.security.Principal;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.contact.dao.ContactRepository;
 import com.contact.dao.UserRepository;
 import com.contact.entities.Contact;
 import com.contact.entities.User;
@@ -33,17 +35,18 @@ public class CustomerController {
 
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private ContactRepository contactRepository;
 
 	@GetMapping("/data")
 	public String customer_dashboard(Model model, Principal principal) {
 
 		// Getting Username
 		String username = principal.getName();
-		System.out.println("Username" + username);
 
 		// Getting UserDetail
 		User user = userRepository.getUserByEmail(username);
-		System.out.println("User: " + user);
 
 		model.addAttribute("user", user);
 		model.addAttribute("title", "Customer Dashboard - Smart Contact Manager");
@@ -206,5 +209,24 @@ public class CustomerController {
 
 	        return "redirect:/customer/addContact";
 	    }
+	}
+	
+	// View All Contact
+	@GetMapping("/viewContact")
+	public String viewContact(Model model, Principal principal) {
+		
+		String username = principal.getName();
+		System.out.println("Username: "+username);
+		
+		User user = userRepository.getUserByEmail(username);
+		
+		model.addAttribute("user", user);
+		model.addAttribute("title", "Customer View Contact - Smart Contact Manager");
+	
+		// Read Data from Database
+		List<Contact> list = contactRepository.getContactById(user.getId());
+		System.out.println(list);
+		
+		return "customer/customer_view_contact";
 	}
 }
